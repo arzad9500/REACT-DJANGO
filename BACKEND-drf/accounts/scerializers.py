3eec2adc26ee36eae77b938be2e1fd03a4d83302
arzad9500 <arzad3431@gmail.com>
2025-12-16 16:03:00 +0530

    till serializer ans views are done and check project api in browser

---
+++ BACKEND-drf/accounts/scerializers.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+# first line 
+# This line imports the User model provided by Django.
+# User is a built-in model
+# It handle authentication and authorization
+# Stored in Django’s default database
+
+class Userserializers (serializers.ModelSerializer):
+    password =  serializers.CharField(write_only=True,style={"input_type":"password"}, min_length=8)
+    class Meta:
+        model =User
+        fields =["username","email","password"]  # this is the order
+
+    def create(self, validated_data):
+        # User.objects. create = save the password in a plain text
+        # User.objects.create_user = automatically hash the password in db
+        user = User.objects.create_user(
+        validated_data['username'],
+        validated_data['email'], # , validated_data.get("email"),  # optional , if not get return none
+        validated_data['password']
+        )
+        # user = User.objects.create_user(**validated_data)
+        return user
---
+++ BACKEND-drf/accounts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .scerializers import *
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+
+
+class RegisterView (generics.CreateAPIView):
+
+    queryset = User.objects.all() 
+    serializer_class = Userserializers
+    permission_classes = [AllowAny]     # can access by anyone
